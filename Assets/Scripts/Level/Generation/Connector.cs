5f814b94416cfe872a59c23804bd118ac2d53830
V4lonforth <v4lonforth@gmail.com>
2020-04-09 18:27:47 +0300

    Fixed doors

--- Assets/Scripts/Level/Generation/Connector.cs
+++ Assets/Scripts/Level/Generation/Connector.cs
@@ -1,76 +1,60 @@
-﻿using System.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Connector : MonoBehaviour
 {
     public void Connect(Tower tower)
     {
-        int width = tower.Size.x;
         foreach (Room room in tower.Rooms)
             foreach (Room adjacentRoom in room.AdjacentRooms)
                 if (!room.ConnectedRooms.Contains(adjacentRoom))
-                    ConnectRooms(room, adjacentRoom, width);
+                    ConnectRooms(room, adjacentRoom);
     }
 
-    private void ConnectRooms(Room first, Room second, int width)
+    private void ConnectRooms(Room first, Room second)
     {
-        if (first.ConnectedRooms.Contains(second))
-        {
-            return;
-        }
-        /*if (first.ConnectedRooms.Count > 1)
-        {
-            int minLevel = Mathf.Min(first.LowestLevel, second.LowestLevel);
-            int maxLevel = Mathf.Max(first.HighestLevel, second.HighestLevel);
-            int[] cellCounts = new int[maxLevel - minLevel + 1];
-            bool[,] roomHasCell = new bool[maxLevel - minLevel + 1, 2];
-
-            first.Cells.ForEach(cell => { cellCounts[cell.Position.y - minLevel]++; roomHasCell[cell.Position.y - minLevel, 0] = true; });
-            second.Cells.ForEach(cell => { cellCounts[cell.Position.y - minLevel]++; roomHasCell[cell.Position.y - minLevel, 1] = true; });
-            for (int i = 0; i < cellCounts.Length; i++)
-            {
-                if (cellCounts[i] >= width)
-                {
-                    if (roomHasCell[i, 0] && roomHasCell[i, 1])
-                    {
-                        Room highestRoom = first.HighestLevel > second.HighestLevel ? first : first.HighestLevel < second.HighestLevel ? second : null;
-                        if (highestRoom)
-                        {
-                            Room otherRoom = ReferenceEquals(first, highestRoom) ? second : first;
-                            if (GetHighestRow(otherRoom, width).All(cell => !cell || cell.AdjacentRooms.Contains(highestRoom)))
-                                break;
-                        }
-                        Room lowestRoom = first.LowestLevel < second.LowestLevel ? first : first.LowestLevel > second.LowestLevel ? second : null;
-                        if (lowestRoom)
-                        {
-                            Room otherRoom = ReferenceEquals(first, lowestRoom) ? second : first;
-                            if (GetLowestRow(otherRoom, width).All(cell => !cell || cell.AdjacentRooms.Contains(lowestRoom)))
-                                break;
-                        }
-                        return;
-                    }
-                    else
-                        break;
-                }
-            }
-        }*/
-
         List<Cell> adjacentCells = first.Cells.FindAll(cell => cell.AdjacentRooms.Contains(second));
 
-        Cell firstCell = adjacentCells[Random.Range(0, adjacentCells.Count)];
-        foreach (Direction direction in Direction.Straights)
-            if (!(firstCell.AdjacentCells[direction] is null) && ReferenceEquals(firstCell.AdjacentCells[direction].Room, second))
-            {
-                ConnectCells(firstCell, firstCell.AdjacentCells[direction], direction);
-                break;
-            }
+        while (adjacentCells.Count > 0)
+        {
+            int index = Random.Range(0, adjacentCells.Count);
+            Cell cell = adjacentCells[index];
+
+            foreach (Direction direction in Direction.Straights)
+                if (!(cell.AdjacentCells[direction] is null) && ReferenceEquals(cell.AdjacentCells[direction].Room, second))
+                {
+                    ConnectCells(cell, cell.AdjacentCells[direction], direction);
+                    return;
+                }
+
+            adjacentCells.RemoveAt(index);
+        }
+    }
+
+    private void ConnectDiagonals(Cell first, Cell second, Direction direction)
+    {
+        Cell clockwiseCell = first.AdjacentCells[direction.Clockwise];
+        if (clockwiseCell != null && clockwiseCell.Room == second.Room)
+        {
+            first.ConnectedCells[direction.Clockwise] = clockwiseCell;
+            clockwiseCell.ConnectedCells[direction.Clockwise.Opposite] = first;
+        }
+
+        Cell counterclockwise = first.AdjacentCells[direction.Counterclockwise];
+        if (counterclockwise != null && counterclockwise.Room == second.Room)
+        {
+            first.ConnectedCells[direction.Counterclockwise] = counterclockwise;
+            counterclockwise.ConnectedCells[direction.Counterclockwise.Opposite] = first;
+        }
     }
 
     private void ConnectCells(Cell first, Cell second, Direction direction)
     {
         first.ConnectedCells[direction] = second;
         second.ConnectedCells[direction.Opposite] = first;
+
+        ConnectDiagonals(first, second, direction);
+        ConnectDiagonals(second, first, direction.Opposite);
 
         first.Room.ConnectedRooms.Add(second.Room);
         second.Room.ConnectedRooms.Add(first.Room);
