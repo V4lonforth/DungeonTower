b5cd83185b15d172154ebdfa68c0a34ec115bbcf
Aiguzin, Ivan <ivan.aiguzin@arcadia.spb.ru>
2021-04-10 18:36:22 +0300

    Add menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void NewGame()
+        {
+            SceneManager.LoadScene("NewLevel");
+        }
+
+        public void OpenSettings()
+        {
+            SceneManager.LoadScene("Settings");
+        }
+
+        public void Exit()
+        {
+            Debug.Log("Exit...");
+            Application.Quit();
+        }
+    }
+}
